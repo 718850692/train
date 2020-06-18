fadf6f2b7f385527c68bc2ea03c17e4cf83af623
xiaoer985 <718850692@qq.com>
2020-06-18 21:48:57 +0800

    修改

--- danmu/danmu.js
+++ danmu/danmu.js
@@ -1,26 +1,30 @@
-
-            
-            $(function() {
+$(function() {
                 
-                $('.send').click(function() {    //设置“发送”按钮点击事件，将弹幕体显示在弹幕墙上
+   $('.send').click(function() {    //设置“发送”按钮点击事件，将弹幕体显示在弹幕墙上
                    
-                    var val = $('.content').val();  //获取文本输入框的内容
-              
-                    
-                    var $content = $('<div class="text">' + val + '</div>');//将文本框内容用div包裹起来，便于后续处理
+    var val = $('.content').val();  //获取文本输入框的内容            
+    var $content = $('<div class="text">' + val + '</div>');//将文本框内容用div包裹起来，便于后续处理
                   
-                    $screen = $(document.getElementById("screen"));  //获取弹幕墙对象
-                    
-                    var top = Math.random() * $screen.height()+40;//设置弹幕体出现时的上边距，为任意值
+        $screen = $(document.getElementById("screen"));  //获取弹幕墙对象
 
-                    $content.css({      //设置弹幕体的上边距和右边距
-                        top: top + "px",
-                        right: 80,  
-                       "font-size":"150%",
-                       "color":"red"
+        // 设置字体的随机性质
+        var fontColor = "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random()) + ")";
+        var fontSize = Math.floor((Math.random()* 20) + 15) + "px";
+        var right = $(".screen_container").width() + "px";
+        var top = Math.floor(Math.random() * 400) + "px";
+           top = parseInt(top) > 352 ? "352px" : top;
+                   
+           
+        $content.css({          //设置弹幕出现时的样子
+                        "position": 'absolute',
+                        "color": fontColor,
+                        "font-size": fontSize,
+                        "right": 80,
+                        "top": top,
+                        "whiteSpace": 'nowrap',
+                        "display": 'block',
                     });
-            
-                    
+ 
                     $('.dm_show').append($content);    //将弹幕体添加到弹幕墙中
                     
                     $content.animate({      //弹幕体从右到左，出现时间
